8c44fee81c89dc418816281989157e2fc73c0e81
ayush-sharma-codes-stack <aayushsharmahrx@gmail.com>
2026-04-29 21:37:33 +0530

    last fix

--- api/index.js
+++ api/index.js
@@ -60,8 +60,9 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () =>
+    console.log(`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  );
+}
 export default app;
